9d2b3978cab76990277f3481640436b620a63904
yongxu <yongxu@student.ethz.ch>
2022-04-17 23:15:57 +0200

    clean test files

--- clean.sh
+++ clean.sh
@@ -2,3 +2,10 @@ cd ./PolicyLog/server
 
 rm PL_logServer
 rm PL_logSigner
+
+cd ../../
+rm -r ./fileTransfer/PCA/RPC/*
+rm -r ./fileTransfer/PCA/ValidRPC/*
+rm -r ./fileTransfer/PolicyLog/treesConfig/*
+rm -r ./fileTransfer/PolicyLog/logRoot/*
+
