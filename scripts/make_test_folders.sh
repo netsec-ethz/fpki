d46f197d9f4b7fccb8fdfca06b09c736c23c5484
yongxu <yongxu@student.ethz.ch>
2022-04-25 23:58:19 +0200

    clean up

---
+++ scripts/make_test_folders.sh
@@ -0,0 +1,5 @@
+mkdir ../tests/intergration_tests/policylog_interaction/testdata/output/trees_config/
+mkdir ../tests/intergration_tests/domainowner_pca_policlog_interaction/file_exchange/policylog/logRoot/
+mkdir ../tests/intergration_tests/domainowner_pca_policlog_interaction/file_exchange/pcaoutput/rpc/
+mkdir ../tests/intergration_tests/domainowner_pca_policlog_interaction/file_exchange/policylog/spt/
+mkdir ../tests/intergration_tests/domainowner_pca_policlog_interaction/file_exchange/policylog/trees_config/
