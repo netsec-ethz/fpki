6e8c5a594fa5937585f1f08bae6a3e9f078d69d0
yongxu <yongxu@student.ethz.ch>
2022-06-02 18:07:59 +0200

    more test for responder

--- pkg/mapserver/responder/responder_test.go
+++ pkg/mapserver/responder/responder_test.go
@@ -13,3 +13,17 @@ func TestDomainName(t *testing.T) {
 	require.NoError(t, err, "parseDomainName error")
 	assert.Equal(t, 1, len(result), "length error")
 }
+
+func TestGetMapping(t *testing.T) {
+	testDomains := []string{"a.google.com", "b.google.com", "c.google.com", "a.baidu.com"}
+	domainResultMap, domainProofMap, err := getMapping(testDomains)
+	require.NoError(t, err, "getMapping error")
+
+	assert.Equal(t, 4, len(domainResultMap), "domainResultMap length error")
+
+	proofs, ok := domainResultMap["a.google.com"]
+	assert.True(t, ok, "domain not contained")
+	assert.Equal(t, 2, len(proofs), "proofs length error")
+
+	assert.Equal(t, 6, len(domainProofMap), "domainProofMap length error")
+}
