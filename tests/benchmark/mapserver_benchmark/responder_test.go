5b2471110c19a5825d04c52fb091f866e3fff214
Juan A. Garcia Pardo <juagargi@gmail.com>
2022-06-16 13:52:57 +0200

    responder issue: check UT

---
+++ tests/benchmark/mapserver_benchmark/responder_test.go
@@ -0,0 +1,67 @@
+package benchmark
+
+import (
+	"context"
+	"testing"
+	"time"
+
+	_ "github.com/go-sql-driver/mysql"
+	"github.com/netsec-ethz/fpki/pkg/mapserver/common"
+	"github.com/netsec-ethz/fpki/pkg/mapserver/logpicker"
+	"github.com/netsec-ethz/fpki/pkg/mapserver/responder"
+	"github.com/netsec-ethz/fpki/pkg/mapserver/updater"
+	"github.com/stretchr/testify/require"
+)
+
+func TestResponder(t *testing.T) {
+	mapUpdater, err := updater.NewMapUpdater(nil, 233)
+	require.NoError(t, err)
+	ctx, cancelF := context.WithTimeout(context.Background(), 15*time.Minute)
+	defer cancelF()
+
+	mapUpdater.Fetcher.BatchSize = 10000
+	const baseCTSize = 2 * 1000
+	const count = 2
+	mapUpdater.StartFetching("https://ct.googleapis.com/logs/argon2021",
+		baseCTSize, baseCTSize+count-1)
+
+	n, err := mapUpdater.UpdateNextBatch(ctx)
+	require.NoError(t, err)
+	require.Equal(t, n, count)
+
+	n, err = mapUpdater.UpdateNextBatch(ctx)
+	require.NoError(t, err)
+	require.Equal(t, n, 0)
+
+	root := mapUpdater.GetRoot()
+	err = mapUpdater.Close()
+	require.NoError(t, err)
+
+	// manually get those certificates and make a list of the common names
+	// https://ct.googleapis.com/logs/argon2021/ct/v1/get-entries?start=2000&end=2001
+	fetcher := logpicker.LogFetcher{
+		URL:         "https://ct.googleapis.com/logs/argon2021",
+		Start:       baseCTSize,
+		End:         baseCTSize + count - 1,
+		WorkerCount: 1,
+		BatchSize:   20,
+	}
+	certs, err := fetcher.FetchAllCertificates(ctx)
+	require.NoError(t, err)
+	require.Len(t, certs, count)
+	names := make([]string, len(certs))
+	for i, c := range certs {
+		names[i] = c.Subject.CommonName
+	}
+
+	// create responder and request proof for those names
+	responder, err := responder.NewMapResponder(ctx, root, 233, 10)
+	require.NoError(t, err)
+	for _, name := range names {
+		responses, err := responder.GetProof(ctx, name)
+		require.NoError(t, err)
+		for _, r := range responses {
+			require.Equal(t, common.PoP, r.PoI.ProofType)
+		}
+	}
+}
