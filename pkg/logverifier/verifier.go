286a602d283826f8dd966856e5b394bc853bf093
Juan A. Garcia Pardo <juagargi@gmail.com>
2023-01-13 10:38:04 +0100

    Cleanup some verify functions.

--- pkg/logverifier/verifier.go
+++ pkg/logverifier/verifier.go
@@ -1,7 +1,6 @@
 package logverifier
 
 import (
-	"errors"
 	"fmt"
 
 	"github.com/google/trillian"
@@ -17,7 +16,7 @@ type LogVerifier struct {
 	hasher merkle.LogHasher
 }
 
-//NewLogVerifier: retutn a new log verifier
+// NewLogVerifier: return a new log verifier
 func NewLogVerifier(hasher merkle.LogHasher) *LogVerifier {
 	if hasher == nil {
 		hasher = rfc6962.DefaultHasher
@@ -35,8 +34,10 @@ func (logVerifier *LogVerifier) HashLeaf(input []byte) []byte {
 
 // VerifyInclusionWithPrevLogRoot: This function verify the leaf using an old log root(tree head)
 // Logic: Verify the leaf using old log root -> verify the old root using the newest root
-func (c *LogVerifier) VerifyInclusionWithPrevLogRoot(trusted *types.LogRootV1, newRoot *types.LogRootV1,
-	consistency [][]byte, leafHash []byte, proof []*trillian.Proof) error {
+func (c *LogVerifier) VerifyInclusionWithPrevLogRoot(trusted *types.LogRootV1,
+	newRoot *types.LogRootV1, consistency [][]byte, leafHash []byte,
+	proof []*trillian.Proof) error {
+
 	switch {
 	case trusted == nil:
 		return fmt.Errorf("VerifyInclusionWithPrevLogRoot() error: trusted == nil")
@@ -59,17 +60,19 @@ func (c *LogVerifier) VerifyInclusionWithPrevLogRoot(trusted *types.LogRootV1, n
 
 // VerifyRoot: verifies that newRoot is a valid append-only operation from
 // trusted root. If trusted.TreeSize is zero, a consistency proof is not needed.
-func (c *LogVerifier) VerifyRoot(trusted *types.LogRootV1,
-	newRoot *types.LogRootV1, consistency [][]byte) (*types.LogRootV1, error) {
+func (c *LogVerifier) VerifyRoot(trusted *types.LogRootV1, newRoot *types.LogRootV1,
+	consistency [][]byte) (*types.LogRootV1, error) {
+
 	switch {
 	case trusted == nil:
 		return nil, fmt.Errorf("VerifyRoot() error: trusted == nil")
 	case newRoot == nil:
 		return nil, fmt.Errorf("VerifyRoot() error: newRoot == nil")
 	case trusted.TreeSize != 0:
-		// Verify consistency proof.
-		if err := logProof.VerifyConsistency(c.hasher, trusted.TreeSize, newRoot.TreeSize, consistency, trusted.RootHash,
-			newRoot.RootHash); err != nil {
+		// Verify consistency proof:
+		if err := logProof.VerifyConsistency(c.hasher, trusted.TreeSize, newRoot.TreeSize,
+			consistency, trusted.RootHash, newRoot.RootHash); err != nil {
+
 			return nil, fmt.Errorf("failed to verify consistency proof from %d->%d %x->%x: %v",
 				trusted.TreeSize, newRoot.TreeSize, trusted.RootHash, newRoot.RootHash, err)
 		}
@@ -81,6 +84,7 @@ func (c *LogVerifier) VerifyRoot(trusted *types.LogRootV1,
 // matches the given trusted root.
 func (c *LogVerifier) VerifyInclusionByHash(trusted *types.LogRootV1, leafHash []byte,
 	proofs []*trillian.Proof) error {
+
 	switch {
 	case trusted == nil:
 		return fmt.Errorf("VerifyInclusionByHash() error: trusted == nil")
@@ -88,53 +92,43 @@ func (c *LogVerifier) VerifyInclusionByHash(trusted *types.LogRootV1, leafHash [
 		return fmt.Errorf("VerifyInclusionByHash() error: proof == nil")
 	}
 
-	isVerified := false
-	// as long as one proof is verified, the verification is successful.
-	// Proofs might contains multiple proof for different leaves, while the content of each leaf is identical.
-	// Trillian will return all the proofs for one content. So one successful verification is enough.
+	// As long as one proof is verified, the verification is successful.
+	// Proofs might contain multiple proofs for different leaves, while the content of each leaf
+	// is identical. Trillian will return all the proofs for one content.
+	// So one successful verification is enough.
 	for _, proof := range proofs {
-		err := logProof.VerifyInclusion(c.hasher, uint64(proof.LeafIndex), trusted.TreeSize, leafHash,
-			proof.Hashes, trusted.RootHash)
-		if err == nil {
-			isVerified = true
-			break
+		if err := logProof.VerifyInclusion(c.hasher, uint64(proof.LeafIndex), trusted.TreeSize,
+			leafHash, proof.Hashes, trusted.RootHash); err == nil {
+
+			return nil
 		}
 	}
-	if !isVerified {
-		return errors.New("Verificate fails!")
-	}
-	return nil
+	return fmt.Errorf("verification fails")
 }
 
 func (c *LogVerifier) VerifySP(sp *common.SP) error {
-	spCopy := &common.SP{Policies: sp.Policies,
-		TimeStamp:         sp.TimeStamp,
-		Subject:           sp.Subject,
-		CAName:            sp.CAName,
-		SerialNumber:      sp.SerialNumber,
-		CASignature:       sp.CASignature,
-		RootCertSignature: sp.RootCertSignature}
-
-	serialisedStruc, err := common.JsonStrucToBytes(spCopy)
+	// Get the hash of the SP without SPTs:
+	SPTs := sp.SPTs
+	sp.SPTs = []common.SPT{}
+	serializedStruct, err := common.JsonStrucToBytes(sp)
 	if err != nil {
-		return fmt.Errorf("VerifySP | JsonStrucToBytes | %w", err)
+		return fmt.Errorf("VerifyRPC | JsonStrucToBytes | %w", err)
 	}
-
-	bytesHash := c.HashLeaf([]byte(serialisedStruc))
+	bytesHash := c.HashLeaf([]byte(serializedStruct))
+	// Restore the SPTs to the SP:
+	sp.SPTs = SPTs
 
 	for _, p := range sp.SPTs {
 		sth, err := common.JsonBytesToLogRoot(p.STH)
 		if err != nil {
 			return fmt.Errorf("VerifySP | JsonBytesToLogRoot | %w", err)
 		}
-
 		poi, err := common.JsonBytesToPoI(p.PoI)
 		if err != nil {
 			return fmt.Errorf("VerifySP | JsonBytesToPoI | %w", err)
 		}
 
-		err = c.VerifyInclusionByHash(sth, bytesHash, poi)
-		if err != nil {
+		if err = c.VerifyInclusionByHash(sth, bytesHash, poi); err != nil {
 			return fmt.Errorf("VerifySP | VerifyInclusionByHash | %w", err)
 		}
 	}
@@ -142,39 +136,28 @@ func (c *LogVerifier) VerifySP(sp *common.SP) error {
 }
 
 func (c *LogVerifier) VerifyRPC(rpc *common.RPC) error {
-	rpcCopy := &common.RPC{SerialNumber: rpc.SerialNumber,
-		Subject:            rpc.Subject,
-		Version:            rpc.Version,
-		PublicKeyAlgorithm: rpc.PublicKeyAlgorithm,
-		PublicKey:          rpc.PublicKey,
-		NotBefore:          rpc.NotBefore,
-		NotAfter:           rpc.NotAfter,
-		CAName:             rpc.CAName,
-		SignatureAlgorithm: rpc.SignatureAlgorithm,
-		TimeStamp:          rpc.TimeStamp,
-		PRCSignature:       rpc.PRCSignature,
-		CASignature:        rpc.CASignature}
-
-	serialisedStruc, err := common.JsonStrucToBytes(rpcCopy)
+	// Get the hash of the RPC without SPTs:
+	SPTs := rpc.SPTs
+	rpc.SPTs = []common.SPT{}
+	serializedStruct, err := common.JsonStrucToBytes(rpc)
 	if err != nil {
 		return fmt.Errorf("VerifyRPC | JsonStrucToBytes | %w", err)
 	}
-
-	bytesHash := c.HashLeaf([]byte(serialisedStruc))
+	bytesHash := c.HashLeaf([]byte(serializedStruct))
+	// Restore the SPTs to the RPC:
+	rpc.SPTs = SPTs
 
 	for _, p := range rpc.SPTs {
 		sth, err := common.JsonBytesToLogRoot(p.STH)
 		if err != nil {
 			return fmt.Errorf("VerifyRPC | JsonBytesToLogRoot | %w", err)
 		}
-
 		poi, err := common.JsonBytesToPoI(p.PoI)
 		if err != nil {
 			return fmt.Errorf("VerifyRPC | JsonBytesToPoI | %w", err)
 		}
 
-		err = c.VerifyInclusionByHash(sth, bytesHash, poi)
-		if err != nil {
+		if err = c.VerifyInclusionByHash(sth, bytesHash, poi); err != nil {
 			return fmt.Errorf("VerifyRPC | VerifyInclusionByHash | %w", err)
 		}
 	}
