790edc4d00dedbc0a9347d0c0dd79b847ab566d6
Juan A. Garcia Pardo <juagargi@gmail.com>
2023-02-02 10:58:10 +0100

    catch sigint

--- cmd/ingest/main.go
+++ cmd/ingest/main.go
@@ -5,8 +5,10 @@ import (
 	"fmt"
 	"io/ioutil"
 	"os"
+	"os/signal"
 	"path/filepath"
 	"runtime/pprof"
+	"syscall"
 
 	"github.com/netsec-ethz/fpki/pkg/db"
 )
@@ -33,21 +35,32 @@ func mainFunction() int {
 	}
 
 	// Profiling:
+	stopProfiles := func() {
+		if *cpuProfile != "" {
+			pprof.StopCPUProfile()
+		}
+		if *memProfile != "" {
+			f, err := os.Create(*memProfile)
+			exitIfError(err)
+			err = pprof.WriteHeapProfile(f)
+			exitIfError(err)
+		}
+	}
 	if *cpuProfile != "" {
 		f, err := os.Create(*cpuProfile)
 		exitIfError(err)
 		err = pprof.StartCPUProfile(f)
 		exitIfError(err)
-		defer pprof.StopCPUProfile()
 	}
-	if *memProfile != "" {
-		defer func() {
-			f, err := os.Create(*memProfile)
-			exitIfError(err)
-			err = pprof.WriteHeapProfile(f)
-			exitIfError(err)
-		}()
-	}
+	defer stopProfiles()
+
+	// Signals catching:
+	signals := make(chan os.Signal)
+	signal.Notify(signals, syscall.SIGINT, syscall.SIGTERM)
+	go func() {
+		<-signals
+		stopProfiles()
+	}()
 
 	conn, err := db.Connect(nil)
 	exitIfError(err)
