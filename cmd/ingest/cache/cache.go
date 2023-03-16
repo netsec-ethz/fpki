92e22fea47eb42fdc8e4830752eabd373b60838f
Juan A. Garcia Pardo <juagargi@gmail.com>
2023-03-16 16:04:36 +0100

    New caches package for ingest.
    It contains the principal/shadow and the nocache caches.

---
+++ cmd/ingest/cache/cache.go
@@ -0,0 +1,12 @@
+package cache
+
+import "github.com/netsec-ethz/fpki/pkg/common"
+
+type Cache interface {
+	Contains(*common.SHA256Output) bool
+	AddIDs([]*common.SHA256Output)
+}
+
+func NewNoCache() NoCache {
+	return NoCache{}
+}
