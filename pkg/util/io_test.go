eee588db9fdff336e64c5e32d1271e84a44b9daa
Juan A. Garcia Pardo <juagargi@gmail.com>
2023-04-07 01:29:39 +0200

    Finish changing PEM functions.

--- pkg/util/io_test.go
+++ pkg/util/io_test.go
@@ -19,24 +19,3 @@ func TestNewGzipReader(t *testing.T) {
 	err = r.Close()
 	require.NoError(t, err)
 }
-
-func TestLoadCertsWithPEMReader(t *testing.T) {
-	r, err := NewGzipReader("../../tests/testdata/certs.pem.gz")
-	require.NoError(t, err)
-
-	certs, err := LoadCertsWithPEMReader(r)
-	require.NoError(t, err)
-	require.Len(t, certs, 100000, "wrong length %d", len(certs))
-
-	err = r.Close()
-	require.NoError(t, err)
-}
-
-func TestLoadCertsFromPEMBuffer(t *testing.T) {
-	raw, err := ReadAllGzippedFile("../../tests/testdata/certs.pem.gz")
-	require.NoError(t, err)
-
-	certs, err := LoadCertsFromPEMBuffer(raw)
-	require.NoError(t, err)
-	require.Len(t, certs, 100000, "wrong length %d", len(certs))
-}
