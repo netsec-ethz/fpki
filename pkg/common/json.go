465347b0808f92568237fe5e2af1ef436c1ea4ad
Juan A. Garcia Pardo <juagargi@gmail.com>
2023-05-03 22:15:08 +0200

    PolicyObjectBase has a Raw field with []byte.

--- pkg/common/json.go
+++ pkg/common/json.go
@@ -11,7 +11,8 @@ import (
 )
 
 type serializableObjectBase struct {
-	O any
+	O       any  // actual object to Marshal/Unmarshal
+	skipRaw bool // flag controlling JSON copying into PolicyObjectBase.Raw
 }
 
 func ToJSON(obj any) ([]byte, error) {
@@ -23,10 +24,21 @@ func ToJSON(obj any) ([]byte, error) {
 	return json.Marshal(obj)
 }
 
-func FromJSON(data []byte) (any, error) {
+func FromJSON(data []byte, opts ...FromJSONModifier) (any, error) {
 	var base serializableObjectBase
+	for _, mod := range opts {
+		mod(&base)
+	}
 	err := json.Unmarshal(data, &base)
 	return base.O, err
+}
+
+type FromJSONModifier func(*serializableObjectBase)
+
+// WithSkipCopyJSONIntoPolicyObjects avoids copying the raw JSON into each one of the
+// objects that aggregate a PolicyObjectBase (RPC, SP, etc).
+func WithSkipCopyJSONIntoPolicyObjects(o *serializableObjectBase) {
+	o.skipRaw = true
 }
 
 func (o serializableObjectBase) MarshalJSON() ([]byte, error) {
@@ -69,7 +81,7 @@ func (*serializableObjectBase) marshalJSON(obj any) (string, []byte, error) {
 		T = "logrootv1"
 	default:
 		valOf := reflect.ValueOf(obj)
-		switch valOf.Type().Kind() {
+		switch valOf.Kind() {
 		case reflect.Pointer:
 			// Dereference and convert to "any".
 			T, O, err := (*serializableObjectBase)(nil).marshalJSON(valOf.Elem().Interface())
@@ -106,25 +118,55 @@ func (o *serializableObjectBase) UnmarshalJSON(data []byte) error {
 		return err
 	}
 	// Parse the T,O that we received.
-	ok, obj, err := unmarshalTypeObject(tmp.T, tmp.O)
-	if !ok {
-		if len(tmp.T) > 0 && tmp.T[0] == '*' {
-			// Pointer, try again just once.
-			tmp.T = tmp.T[1:]
-			_, obj, err = unmarshalTypeObject(tmp.T, tmp.O)
-			// Now convert to a pointer to the original object.
-			objPtr := reflect.New(reflect.TypeOf(obj))
-			objPtr.Elem().Set(reflect.ValueOf(obj)) // assign original object
-			obj = objPtr.Interface()
+	wasPtr := false
+	ok, obj, err := o.unmarshalTypeObject(tmp.T, tmp.O)
+	if !ok && len(tmp.T) > 0 && tmp.T[0] == '*' {
+		// It looks like a pointer, try again just once.
+		wasPtr = true
+		tmp.T = tmp.T[1:] // Remove the *
+		ok, obj, err = o.unmarshalTypeObject(tmp.T, tmp.O)
+	}
+
+	// Almost everything is done now. We should 1. do a obj = &obj and 2. copy the raw JSON
+	// into the Raw field of the structure.
+	shouldCopyJSON := !o.skipRaw && reflect.ValueOf(obj).Kind() != reflect.Slice
+	if ok && (wasPtr || shouldCopyJSON) { // skip if no JSON copy and it wasn't a pointer
+		// Until here, obj is never a pointer. Convert obj to a pointer to obj.
+		objPtr := reflect.New(reflect.TypeOf(obj)) // new pointer of T
+		objPtr.Elem().Set(reflect.ValueOf(obj))    // assign original object
+		obj = objPtr.Interface()                   // obj is now a pointer to the original
+
+		// If we should copy JSON to Raw:
+		if shouldCopyJSON {
+			// Find out if the object is a pointer to a PolicyObjectBase like structure.
+			base := reflect.Indirect(reflect.ValueOf(obj)).FieldByName("PolicyObjectBase")
+			if base != (reflect.Value{}) {
+				// It is a PolicyObjectBase like object. Check the Raw field (should always be true).
+				if raw := base.FieldByName("RawJSON"); raw != (reflect.Value{}) {
+					// Set its value to the JSON data.
+					raw.Set(reflect.ValueOf(data))
+				} else {
+					// This should never happen, and the next line should ensure it:
+					_ = PolicyObjectBase{}.RawJSON
+					// But terminate the control flow anyways with a panic.
+					panic("logic error: structure PolicyObjectBase has lost its Raw member")
+				}
+			}
+		}
+
+		// If the object was not a pointer, and it had been converted to a pointer, revert.
+		if !wasPtr {
+			obj = reflect.Indirect(reflect.ValueOf(obj)).Interface()
 		}
 	}
+
 	o.O = obj
 	return err
 }
 
 // unmarshalTypeObject returns true if the function understood the type in T, and the object with
 // the specific type represented by T.
-func unmarshalTypeObject(T string, data []byte) (bool, any, error) {
+func (o *serializableObjectBase) unmarshalTypeObject(T string, data []byte) (bool, any, error) {
 	var obj any
 	var err error
 	switch T {
@@ -140,7 +182,9 @@ func unmarshalTypeObject(T string, data []byte) (bool, any, error) {
 			obj = list
 			for i, objData := range tmp {
 				// Is this an embedded SerializableObjectBase?
-				tmp := serializableObjectBase{}
+				tmp := serializableObjectBase{
+					skipRaw: o.skipRaw,
+				}
 				err = json.Unmarshal(objData, &tmp)
 				if err != nil {
 					err = fmt.Errorf("unmarshaling slice, element at %d failed: %w", i, err)
@@ -177,42 +221,32 @@ func unmarshalTypeObject(T string, data []byte) (bool, any, error) {
 func inflateObj[T any](data []byte) (any, error) {
 	var tmp T
 	err := json.Unmarshal(data, &tmp)
+
 	return tmp, err
 }
 
-//
-//
-//
-//
-//
-//
-//
-//
+// ToJSONFile serializes any supported type to a file, using JSON.
+func ToJSONFile(s any, filePath string) error {
+	bytes, err := ToJSON(s)
+	if err != nil {
+		return fmt.Errorf("JsonStructToFile | ToJSON | %w", err)
+	}
 
-// func JSONToPoI(poiBytes []byte) ([]*trillian.Proof, error) {
-// 	po, err := FromJSON(poiBytes)
-// 	if err != nil {
-// 		return nil, fmt.Errorf("JsonBytesToPoI | Unmarshal | %w", err)
-// 	}
-// 	result, ok := po.([]*trillian.Proof)
-// 	if !ok {
-// 		return nil, fmt.Errorf("JsonFileToPoI | object is %T", po)
-// 	}
-// 	return result, nil
-// }
+	err = ioutil.WriteFile(filePath, bytes, 0644)
+	if err != nil {
+		return fmt.Errorf("JsonStructToFile | WriteFile | %w", err)
+	}
+	return nil
+}
 
-// // JSONToLogRoot: Bytes -> log root in json
-// func JSONToLogRoot(logRootBytes []byte) (*trilliantypes.LogRootV1, error) {
-// 	po, err := FromJSON(logRootBytes)
-// 	if err != nil {
-// 		return nil, fmt.Errorf("JsonBytesToLogRoot | Unmarshal | %w", err)
-// 	}
-// 	result, ok := po.(*trilliantypes.LogRootV1)
-// 	if !ok {
-// 		return nil, fmt.Errorf("JsonFileToLogRoot | object is %T", po)
-// 	}
-// 	return result, nil
-// }
+func FromJSONFile(filePath string) (any, error) {
+	data, err := ioutil.ReadFile(filePath)
+	if err != nil {
+		return nil, err
+	}
+
+	return FromJSON(data)
+}
 
 // JsonFileToRPC: read json files and unmarshal it to Root Policy Certificate
 func JsonFileToRPC(filePath string) (*RPC, error) {
@@ -282,27 +316,4 @@ func JsonFileToSP(filePath string) (*SP, error) {
 		err = fmt.Errorf("JsonFileToSP | object is %T", po)
 	}
 	return o, err
-}
-
-// ToJSONFile serializes any supported type to a file, using JSON.
-func ToJSONFile(s any, filePath string) error {
-	bytes, err := ToJSON(s)
-	if err != nil {
-		return fmt.Errorf("JsonStructToFile | ToJSON | %w", err)
-	}
-
-	err = ioutil.WriteFile(filePath, bytes, 0644)
-	if err != nil {
-		return fmt.Errorf("JsonStructToFile | WriteFile | %w", err)
-	}
-	return nil
-}
-
-func FromJSONFile(filePath string) (any, error) {
-	data, err := ioutil.ReadFile(filePath)
-	if err != nil {
-		return nil, err
-	}
-
-	return FromJSON(data)
 }
