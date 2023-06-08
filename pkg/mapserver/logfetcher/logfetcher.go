ee5a5363f5cafc2a78d045869b92cdace1e29b16
Juan A. Garcia Pardo <juagargi@gmail.com>
2023-06-08 19:43:44 +0200

    New log fetcher based on google's certificate-transparency client.

--- pkg/mapserver/logfetcher/logfetcher.go
+++ pkg/mapserver/logfetcher/logfetcher.go
@@ -2,226 +2,299 @@ package logfetcher
 
 import (
 	"bufio"
-	"bytes"
 	"context"
 	"crypto/rand"
-	"encoding/base64"
-	"encoding/json"
+	"crypto/tls"
 	"fmt"
 	"net/http"
 	"os"
-	"sync"
 	"time"
 
 	ct "github.com/google/certificate-transparency-go"
-	ctTls "github.com/google/certificate-transparency-go/tls"
+	"github.com/google/certificate-transparency-go/client"
+	"github.com/google/certificate-transparency-go/jsonclient"
 	ctx509 "github.com/google/certificate-transparency-go/x509"
+
 	"github.com/netsec-ethz/fpki/pkg/common"
 	"github.com/netsec-ethz/fpki/pkg/domain"
 )
 
-// LogFetcher is used to download batches of certificates. It has state and keeps some routines
-// downloading certificates in the background, trying to prefetch the next two batches.
-// By default it uses 16 workers, and batches of size 1K.
+const defaultServerBatchSize = 128
+const defaultProcessBatchSize = defaultServerBatchSize * 128
+
+const preloadCount = 2 // Number of batches the LogFetcher tries to preload.
+
+// LogFetcher is used to download CT TBS certificates. It has state and keeps some routines
+// downloading certificates in the background, trying to prefetch preloadCount batches.
+// LogFetcher uses the certificate-transparency-go/client from google to do the heavy lifting.
+// The default size of the server side batch is 128, i.e. the server expects queries in blocks
+// of 128 entries.
+// TODO(juagargi) Use lists of CT log servers: check certificate-transparency-go/ctutil/sctcheck
+// or ct/client/ctclient for a full and standard list that may already implement this.
 type LogFetcher struct {
-	URL         string
-	Start       int
-	End         int
-	WorkerCount int
-	BatchSize   int
-	resultChan  chan []*ctx509.Certificate // batches of certificates
-	errChan     chan error
-	stopChan    chan struct{} // tells the workers to stop fetching
+	url   string
+	start int64 // TODO(juagargi) start & end should go into fetch() and not as part as the type.
+	end   int64
+
+	serverBatchSize  int64 // The server requires queries in blocks of this size.
+	processBatchSize int64 // We unblock NextBatch in batches of this size.
+	ctClient         *client.LogClient
+	chanResults      chan *result
+	chanStop         chan struct{} // tells the workers to stop fetching
+	stopping         bool          // Set at the same time than sending to chanStop
+}
+
+type result struct {
+	certs  []*ctx509.Certificate
+	chains [][]*ctx509.Certificate
+	err    error
+}
+
+func NewLogFetcher(url string) (*LogFetcher, error) {
+	httpClient := &http.Client{
+		Timeout: 10 * time.Second,
+		Transport: &http.Transport{
+			TLSHandshakeTimeout:   30 * time.Second,
+			ResponseHeaderTimeout: 30 * time.Second,
+			MaxIdleConnsPerHost:   10,
+			DisableKeepAlives:     false,
+			MaxIdleConns:          100,
+			IdleConnTimeout:       90 * time.Second,
+			ExpectContinueTimeout: 1 * time.Second,
+			TLSClientConfig: &tls.Config{
+				InsecureSkipVerify: true,
+			},
+		},
+	}
+	opts := jsonclient.Options{UserAgent: "ct-go-ctclient/1.0"}
+	ctClient, err := client.New(url, httpClient, opts)
+	if err != nil {
+		return nil, err
+	}
+	return &LogFetcher{
+		url: url,
+
+		serverBatchSize:  defaultServerBatchSize,
+		processBatchSize: defaultProcessBatchSize,
+		ctClient:         ctClient,
+		chanResults:      make(chan *result, preloadCount),
+		chanStop:         make(chan struct{}),
+	}, nil
 }
 
 // StartFetching will start fetching certificates in the background, so that there is
 // at most two batches ready to be immediately read by NextBatch.
-func (f *LogFetcher) StartFetching() {
-	if f.BatchSize == 0 {
-		f.BatchSize = 1000
-	}
-	if f.WorkerCount == 0 {
-		f.WorkerCount = 16
-	}
-	f.resultChan = make(chan []*ctx509.Certificate, 2)
-	f.errChan = make(chan error)
-	f.stopChan = make(chan struct{})
+func (f *LogFetcher) StartFetching(start, end int64) {
+	f.start = start
+	f.end = end
 	go f.fetch()
 }
 
 func (f *LogFetcher) StopFetching() {
-	f.stopChan <- struct{}{}
+	f.stopping = true
+	f.chanStop <- struct{}{}
 }
 
 // NextBatch returns the next batch of certificates as if it were a channel.
 // The call blocks until a whole batch is available. The last batch may have less elements.
 // Returns nil when there is no more batches, i.e. all certificates have been fetched.
-func (f *LogFetcher) NextBatch(ctx context.Context) ([]*ctx509.Certificate, error) {
+func (f *LogFetcher) NextBatch(
+	ctx context.Context,
+) (
+	certs []*ctx509.Certificate,
+	chains [][]*ctx509.Certificate,
+	err error) {
+
 	select {
 	case <-ctx.Done():
 		f.StopFetching()
-		return nil, fmt.Errorf("NextBatch %w", ctx.Err())
-	case certs := <-f.resultChan:
-		return certs, nil
-	case err := <-f.errChan:
-		return nil, err
+		err = fmt.Errorf("NextBatch %w", ctx.Err())
+		return
+	case res, ok := <-f.chanResults:
+		if !ok {
+			// Channel is closed.
+			return
+		}
+		if err = res.err; err != nil {
+			return
+		}
+		certs = res.certs
+		chains = res.chains
+		return
 	}
 }
 
-// FetchAllCertificates will block until all certificates [start,end] have been fetched.
-func (f *LogFetcher) FetchAllCertificates(ctx context.Context) ([]*ctx509.Certificate, error) {
-	f.StartFetching()
-	certs := make([]*ctx509.Certificate, 0, f.End-f.Start+1)
+// FetchAllCertificates will block until all certificates and chains [start,end] have been fetched.
+func (f *LogFetcher) FetchAllCertificates(
+	ctx context.Context,
+	start,
+	end int64,
+) (
+	certs []*ctx509.Certificate,
+	chains [][]*ctx509.Certificate,
+	err error,
+) {
+
+	f.StartFetching(start, end)
+	certs = make([]*ctx509.Certificate, 0, f.end-f.start+1)
+	chains = make([][]*ctx509.Certificate, 0, f.end-f.start+1)
 	for {
-		batch, err := f.NextBatch(ctx)
-		if err != nil {
-			return nil, err
+		bCerts, bChains, bErr := f.NextBatch(ctx)
+		if bErr != nil {
+			err = bErr
+			return
 		}
-		if len(batch) == 0 {
+		if len(bCerts) == 0 {
 			break
 		}
-		certs = append(certs, batch...)
+		certs = append(certs, bCerts...)
+		chains = append(chains, bChains...)
 	}
-	return certs, nil
+	return
 }
 
 func (f *LogFetcher) fetch() {
-	for start, end := f.Start, min(f.End, f.Start+f.BatchSize-1); start <= f.End; start, end =
-		start+f.BatchSize, min(end+f.BatchSize, f.End) {
+	defer close(f.chanResults)
+	defer close(f.chanStop)
 
-		certs, err := getCertificates(f.URL, start, end, f.WorkerCount, f.stopChan)
-		if err != nil {
-			f.errChan <- err
-			return
-		}
-		f.resultChan <- certs
+	if f.start > f.end {
+		return
 	}
-	close(f.errChan)
-	close(f.resultChan)
-	close(f.stopChan)
+	// Taking into account the batchSize, compute the # of calls to getEntriesInBatches.
+	// It calls count-1 times with the batchSize, and 1 time with the remainder.
+	count := 1 + (f.end-f.start)/f.processBatchSize
+
+	leafEntries := make([]*ct.LeafEntry, f.processBatchSize)
+	for i := int64(0); i < count; i++ {
+		start := f.start + i*f.processBatchSize
+		end := min(f.end, start+f.processBatchSize-1)
+		n, err := f.getRawEntriesInBatches(leafEntries, start, end)
+		if err != nil {
+			f.chanResults <- &result{
+				err: err,
+			}
+			return // Don't continue processing when errors.
+		}
+		certEntries := make([]*ctx509.Certificate, n)
+		chainEntries := make([][]*ctx509.Certificate, n)
+		// Parse each entry to certificates and chains.
+		for i, leaf := range leafEntries[:n] {
+			index := f.start + int64(i)
+			raw, err := ct.RawLogEntryFromLeaf(index, leaf)
+			if err != nil {
+				f.chanResults <- &result{
+					err: err,
+				}
+				return
+			}
+			// Certificate.
+			cert, err := ctx509.ParseCertificate(raw.Cert.Data)
+			if err != nil {
+				f.chanResults <- &result{
+					err: err,
+				}
+				return
+			}
+			certEntries[i] = cert
+			// Chain.
+			chainEntries[i] = make([]*ctx509.Certificate, len(raw.Chain))
+			for j, c := range raw.Chain {
+				chainEntries[i][j], err = ctx509.ParseCertificate(c.Data)
+				if err != nil {
+					f.chanResults <- &result{
+						err: err,
+					}
+					return
+				}
+			}
+		}
+		// Send the result.
+		f.chanResults <- &result{
+			certs:  certEntries,
+			chains: chainEntries,
+		}
+	}
 }
 
-// getCertificates fetches certificates from CT log.
-// It will download end - start + 1 certificates, starting at start, and finishing with end.
-func getCertificates(ctURL string, startIndex, endIndex, numOfWorker int, stopChan chan struct{}) (
-	[]*ctx509.Certificate, error) {
+// streamRawEntries fetches certificates from CT log using getCerts.
+// streamRawEntries repeats a call to getCerts as many times as necessary in batches of
+// serverBatchSize.
+// streamRawEntries will download end - start + 1 certificates,
+// starting at start, and finishing with end.
+func (f *LogFetcher) getRawEntriesInBatches(leafEntries []*ct.LeafEntry, start, end int64) (
+	int64, error) {
 
-	count := endIndex - startIndex + 1
-	if count < numOfWorker {
-		numOfWorker = count
-	}
-	if numOfWorker == 0 {
-		return nil, nil
-	}
+	assert(end >= start, "logic error: call to getRawEntriesInBatches with %d and %d", start, end)
+	_ = leafEntries[end-start] // Fail early if wrong size.
 
-	certsCol := make([][]*ctx509.Certificate, numOfWorker)
-	errs := make([]error, numOfWorker)
-	wg := sync.WaitGroup{}
-	wg.Add(numOfWorker)
+	// TODO(juagargi) should we align the calls to serverBatchSize
+	batchCount := (end - start + 1) / f.serverBatchSize
 
-	stride := count / numOfWorker
-	rem := count % numOfWorker
+	// Do batches.
+	for i := int64(0); !f.stopping && i < batchCount; i++ {
+		bStart := start + i*f.serverBatchSize
+		bEnd := bStart + f.serverBatchSize - 1
+		entries := leafEntries[i*f.serverBatchSize : (i+1)*f.serverBatchSize]
 
-	for i := 0; i < rem; i++ {
-		go func(start, end int, certsPtr *[]*ctx509.Certificate, errPtr *error) {
-			defer wg.Done()
-			*certsPtr, *errPtr = getCerts(ctURL, start, end, stopChan)
-		}(startIndex+i*stride, startIndex+(i+1)*stride, &certsCol[i], &errs[i])
-	}
-	for i := rem; i < numOfWorker; i++ {
-		go func(start, end int, certsPtr *[]*ctx509.Certificate, errPtr *error) {
-			defer wg.Done()
-			*certsPtr, *errPtr = getCerts(ctURL, start, end, stopChan)
-		}(startIndex+i*stride, startIndex+(i+1)*stride-1, &certsCol[i], &errs[i])
-	}
-
-	certs := make([]*ctx509.Certificate, 0, count)
-	wg.Wait()
-	for i := 0; i < numOfWorker; i++ {
-		if errs[i] != nil {
-			return nil, errs[i]
+		n, err := f.getRawEntries(entries, bStart, bEnd)
+		if err != nil {
+			return i * f.serverBatchSize, err
 		}
-		certs = append(certs, certsCol[i]...)
+		if f.stopping {
+			return bEnd - start + 1, nil
+		}
+		assert(n == f.serverBatchSize, "bad size in getRawEntriesInBatches")
 	}
-	return certs, nil
+
+	// Do remainder of batches.
+	remStart := batchCount*f.serverBatchSize + start
+	remEnd := end
+	if remEnd >= remStart {
+		// There is a remainder todo.
+		entries := leafEntries[batchCount*f.serverBatchSize : end-start+1]
+		n, err := f.getRawEntries(entries, remStart, remEnd)
+		if err != nil {
+			return batchCount * f.serverBatchSize, err
+		}
+		if f.stopping {
+			return remStart + n, nil
+		}
+		assert(n == remEnd-remStart+1, "bad remainder size in getRawEntriesInBatches")
+	}
+
+	return end - start + 1, nil
 }
 
-// getCerts gets certificates from CT log. It will request all certs in [start,end] (including both)
-func getCerts(ctURL string, start, end int, stopChan chan struct{}) ([]*ctx509.Certificate, error) {
-	allCerts := make([]*ctx509.Certificate, 0, end-start+1)
-	for end >= start {
-		url := fmt.Sprintf(ctURL+"/ct/v1/get-entries?start=%d&end=%d&quot", start, end)
-		resp, err := http.Get(url)
-		if err != nil {
-			return nil, fmt.Errorf("getCerts | http.Get %w", err)
-		}
+// getRawEntries downloads raw entries. It doesn't have a concept of batch size, and  will
+// re-query the server if it didn't return as many entries as requested.
+// The function returns all entries [start,end], both inclusive.
+// It returns the number of retrieved entries, plus maybe an error.
+// The rawEntries must be at least of size end-start+1, or panic.
+func (f *LogFetcher) getRawEntries(
+	leafEntries []*ct.LeafEntry,
+	start,
+	end int64,
+) (int64, error) {
+
+	_ = leafEntries[end-start] // Fail early if the slice is too small.
+
+	for offset := int64(0); offset < end-start+1; {
 		select {
-		case <-stopChan: // requested to stop
-			return nil, nil
+		case <-f.chanStop: // requested to stop
+			return 0, nil
 		default:
 		}
-		newCerts, err := parseCertificatesFromCTLogServerResponse(resp)
+		rsp, err := f.ctClient.GetRawEntries(context.Background(), start+offset, end)
 		if err != nil {
-			return nil, err
+			return offset, err
 		}
-		start += len(newCerts)
-		allCerts = append(allCerts, newCerts...)
-	}
-	return allCerts, nil
-}
-
-// parseCertificatesFromCTLogServerResponse iteratively gets all requested certificates,
-// with as many HTTP requests as necessary.
-func parseCertificatesFromCTLogServerResponse(resp *http.Response) ([]*ctx509.Certificate, error) {
-	type CertLog struct {
-		Entries []struct {
-			LeafInput string `json:"leaf_input"`
-			ExtraData string `json:"extra_data"`
+		for i := int64(0); i < int64(len(rsp.Entries)); i++ {
+			e := rsp.Entries[i]
+			leafEntries[offset+i] = &e
 		}
+		offset += int64(len(rsp.Entries))
 	}
-	buf := new(bytes.Buffer)
-	buf.ReadFrom(resp.Body)
-	var ctCerts CertLog
-	err := json.Unmarshal(buf.Bytes(), &ctCerts)
-	if err != nil {
-		return nil, fmt.Errorf("getCerts | json unmarshal %w\n%s", err, buf.String())
-	}
-
-	certs := make([]*ctx509.Certificate, len(ctCerts.Entries))
-	// parse merkle leaves and append them to the result
-	for i, entry := range ctCerts.Entries {
-		leafBytes, _ := base64.RawStdEncoding.DecodeString(entry.LeafInput)
-		var merkleLeaf ct.MerkleTreeLeaf
-		ctTls.Unmarshal(leafBytes, &merkleLeaf)
-
-		var certificate *ctx509.Certificate
-		switch entryType := merkleLeaf.TimestampedEntry.EntryType; entryType {
-		case ct.X509LogEntryType:
-			certificate, err = ctx509.ParseCertificate(merkleLeaf.TimestampedEntry.X509Entry.Data)
-			if err != nil {
-				switch err.(type) {
-				case ctx509.NonFatalErrors:
-					fmt.Println(err.Error())
-				default:
-					return nil, fmt.Errorf("getCerts | ParseCertificate %w", err)
-				}
-			}
-		case ct.PrecertLogEntryType:
-			certificate, err = ctx509.ParseTBSCertificate(merkleLeaf.TimestampedEntry.PrecertEntry.TBSCertificate)
-			if err != nil {
-				switch err.(type) {
-				case ctx509.NonFatalErrors:
-					fmt.Println(err.Error())
-				default:
-					return nil, fmt.Errorf("getCerts | ParseTBSCertificate %w", err)
-				}
-			}
-		default:
-			return nil, fmt.Errorf("getCerts | CT type unknown %v", entryType)
-		}
-		certs[i] = certificate
-	}
-	return certs, nil
+	return end - start + 1, nil
 }
 
 // GetPCAndRPC: get PC and RPC from url
@@ -273,9 +346,15 @@ func generateRandomBytes() []byte {
 	return token
 }
 
-func min(a, b int) int {
+func min(a, b int64) int64 {
 	if b < a {
 		return b
 	}
 	return a
+}
+
+func assert(cond bool, format string, params ...any) {
+	if !cond {
+		panic(fmt.Errorf(format, params...))
+	}
 }
