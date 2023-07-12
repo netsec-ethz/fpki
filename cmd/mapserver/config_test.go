b1daa6623000e944dec01016d80ff55b7a2bda06
Juan A. Garcia Pardo <juagargi@gmail.com>
2023-07-12 12:27:54 +0200

    Fix config test to use different package than main.

--- cmd/mapserver/config_test.go
+++ cmd/mapserver/config_test.go
@@ -1,16 +1,18 @@
-package main
+package main_test
 
 import (
 	"encoding/json"
 	"testing"
 	"time"
 
-	"github.com/netsec-ethz/fpki/pkg/util"
 	"github.com/stretchr/testify/require"
+
+	main "github.com/netsec-ethz/fpki/cmd/mapserver"
+	"github.com/netsec-ethz/fpki/pkg/util"
 )
 
 func TestConfigJson(t *testing.T) {
-	config := &Config{
+	config := &main.Config{
 		UpdateAt: util.NewTimeOfDay(3, 00, 00, 00),
 		UpdateTimer: util.DurationWrap{
 			Duration: 24 * time.Hour,
@@ -20,7 +22,7 @@ func TestConfigJson(t *testing.T) {
 	data, err := json.Marshal(config)
 	require.NoError(t, err)
 	t.Logf("JSON is: %s", string(data))
-	copy := &Config{}
+	copy := &main.Config{}
 	err = json.Unmarshal(data, &copy)
 	require.NoError(t, err)
 }
