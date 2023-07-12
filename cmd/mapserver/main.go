55886fa19d05a47667d165e2c11d63d459078415
Juan A. Garcia Pardo <juagargi@gmail.com>
2023-07-12 12:58:27 +0200

    Configure connection to DB.

--- cmd/mapserver/main.go
+++ cmd/mapserver/main.go
@@ -8,6 +8,8 @@ import (
 	"syscall"
 	"time"
 
+	"github.com/netsec-ethz/fpki/pkg/db"
+	"github.com/netsec-ethz/fpki/pkg/db/mysql"
 	"github.com/netsec-ethz/fpki/pkg/util"
 )
 
@@ -38,19 +40,35 @@ func mainFunc() int {
 		return 1
 	}
 
+	var err error
 	if *createSampleConfig {
-		config := &Config{
-			UpdateAt: util.NewTimeOfDay(3, 00, 00, 00),
-			UpdateTimer: util.DurationWrap{
-				Duration: 24 * time.Hour,
-			},
-		}
-		err := WriteConfigurationToFile(flag.Arg(0), config)
-		if err != nil {
-			panic(err)
-		}
+		err = writeSampleConfig()
+	} else {
+		err = run(*updateVar)
 	}
 
+	// We have finished. Print message in case of error.
+	return manageError(err)
+}
+
+func writeSampleConfig() error {
+	dbConfig := db.NewConfig(
+		mysql.WithDefaults(),
+		mysql.WithEnvironment(),
+		mysql.WithLocalSocket("/var/run/mysqld/mysqld.sock"),
+	)
+	config := &Config{
+		UpdateAt: util.NewTimeOfDay(3, 00, 00, 00),
+		UpdateTimer: util.DurationWrap{
+			Duration: 24 * time.Hour,
+		},
+		DB: dbConfig,
+	}
+
+	return WriteConfigurationToFile(flag.Arg(0), config)
+}
+
+func run(updateNow bool) error {
 	ctx := context.Background()
 
 	// Set SIGTERM handler. The context we get is cancelled if one of those signals is caught.
@@ -62,17 +80,11 @@ func mainFunc() int {
 		err = runWithConfig(
 			ctx,
 			config,
-			*updateVar,
+			updateNow,
 		)
 	}
 
-	// We have finished. Print message in case of error.
-	if err != nil {
-		fmt.Fprintln(os.Stderr, err)
-		return 1
-	}
-
-	return 0
+	return err
 }
 
 // runWithConfig examines the configuration, and according to its values, starts a timer to
@@ -83,7 +95,7 @@ func runWithConfig(
 	updateNow bool,
 ) error {
 
-	server, err := NewMapserver()
+	server, err := NewMapserver(c.DB)
 	if err != nil {
 		return err
 	}
@@ -103,4 +115,13 @@ func runWithConfig(
 	<-ctx.Done()
 
 	return nil
+}
+
+func manageError(err error) int {
+	if err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		return 1
+	}
+
+	return 0
 }
