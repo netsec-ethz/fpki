d222154e093476507059d480fe91a725e22926d7
Juan A. Garcia Pardo <juagargi@gmail.com>
2023-10-27 14:19:04 +0200

    Improved RuntimeTest.

--- pkg/tests/defs.go
+++ pkg/tests/defs.go
@@ -13,17 +13,21 @@ type T interface {
 	Name() string
 }
 
-type RuntimeTest struct{}
+type RuntimeTest struct {
+	TestName string
+}
 
 var _ T = RuntimeTest{}
 
-func NewTestObject() RuntimeTest {
-	return RuntimeTest{}
+func NewTestObject(name string) RuntimeTest {
+	return RuntimeTest{
+		TestName: name,
+	}
 }
 
-func (RuntimeTest) Helper()      {}
-func (RuntimeTest) Name() string { return "runtime tester object" }
-func (RuntimeTest) FailNow()     { os.Exit(1) }
+func (RuntimeTest) Helper()         {}
+func (rt RuntimeTest) Name() string { return rt.TestName }
+func (RuntimeTest) FailNow()        { os.Exit(1) }
 func (RuntimeTest) Errorf(format string, args ...interface{}) {
 	fmt.Printf(format, args...)
 }
