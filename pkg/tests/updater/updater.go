f1ca1ed33adea149a31966d58e96d9a2ab8276bc
Juan A. Garcia Pardo <juagargi@gmail.com>
2023-10-27 20:31:02 +0200

    WIP integration test for mapserver.

---
+++ pkg/tests/updater/updater.go
@@ -0,0 +1,82 @@
+package updater
+
+import (
+	"context"
+
+	ctx509 "github.com/google/certificate-transparency-go/x509"
+	"github.com/stretchr/testify/require"
+
+	"github.com/netsec-ethz/fpki/pkg/common"
+	"github.com/netsec-ethz/fpki/pkg/db"
+	"github.com/netsec-ethz/fpki/pkg/mapserver/updater"
+	"github.com/netsec-ethz/fpki/pkg/tests"
+	"github.com/netsec-ethz/fpki/pkg/tests/random"
+	"github.com/netsec-ethz/fpki/pkg/util"
+)
+
+type CertsPoliciesOrBoth int
+
+const (
+	CertsOnly CertsPoliciesOrBoth = iota
+	PoliciesOnly
+	BothCertsAndPolicies
+)
+
+func UpdateDBwithRandomCerts(
+	ctx context.Context,
+	t tests.T,
+	conn db.Conn,
+	domains []string,
+	certsOrPolicies []CertsPoliciesOrBoth,
+) (certs []*ctx509.Certificate, policies []common.PolicyDocument,
+	IDs, parentIDs []*common.SHA256Output, names [][]string) {
+
+	// Prepare the return variables.
+	certs = make([]*ctx509.Certificate, 0, 4*len(domains))
+	IDs = make([]*common.SHA256Output, 0, len(certs))
+	parentIDs = make([]*common.SHA256Output, 0, len(certs))
+	names = make([][]string, 0, len(certs))
+	policies = make([]common.PolicyDocument, 0, len(certs))
+
+	// Generate and insert into DB the requested certificate hierarchies.
+	for i, domain := range domains {
+		var c []*ctx509.Certificate
+		var certIDs, parentCertIDs []*common.SHA256Output
+		var domainNames [][]string
+		var pols []common.PolicyDocument
+
+		// Generate random hierarchies depending of the selection.
+		if certsOrPolicies[i] != PoliciesOnly {
+			c, certIDs, parentCertIDs, domainNames = random.BuildTestRandomCertHierarchy(t, domain)
+		}
+		if certsOrPolicies[i] != CertsOnly {
+			pols = random.BuildTestRandomPolicyHierarchy(t, domain)
+		}
+
+		// Insert all generated hierarchies into DB.
+		err := updater.UpdateWithKeepExisting(ctx, conn, domainNames, certIDs, parentCertIDs,
+			c, util.ExtractExpirations(c), pols)
+		require.NoError(t, err)
+
+		// Add to return variables.
+		certs = append(certs, c...)
+		IDs = append(IDs, certIDs...)
+		parentIDs = append(parentIDs, parentCertIDs...)
+		names = append(names, domainNames...)
+		policies = append(policies, pols...)
+	}
+
+	// Coalescing of payloads.
+	err := updater.CoalescePayloadsForDirtyDomains(ctx, conn)
+	require.NoError(t, err)
+
+	// Create/update the SMT.
+	err = updater.UpdateSMT(ctx, conn)
+	require.NoError(t, err)
+
+	// And cleanup dirty, flagging the end of the update cycle.
+	err = conn.CleanupDirty(ctx)
+	require.NoError(t, err)
+
+	return
+}
---
+++ tests/integration/mapserver/main.go
@@ -0,0 +1,76 @@
+package main
+
+import (
+	"context"
+	"os"
+	"time"
+
+	"github.com/netsec-ethz/fpki/pkg/mapserver"
+	"github.com/netsec-ethz/fpki/pkg/mapserver/config"
+	"github.com/netsec-ethz/fpki/pkg/tests"
+	"github.com/netsec-ethz/fpki/pkg/tests/testdb"
+	tup "github.com/netsec-ethz/fpki/pkg/tests/updater"
+	"github.com/netsec-ethz/fpki/pkg/util"
+	"github.com/stretchr/testify/require"
+)
+
+func main() {
+	os.Exit(mainFunc())
+}
+
+func mainFunc() int {
+	ctx, cancelF := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancelF()
+
+	// Create a test object to be able to later use a test DB.
+	t := tests.NewTestObject("mapserver_integration")
+
+	// Configure a test DB.
+	dbConf, removeF := testdb.ConfigureTestDB(t)
+	defer removeF()
+
+	// Connect to the DB.
+	conn := testdb.Connect(t, dbConf)
+	// Insert some mock data.
+	tup.UpdateDBwithRandomCerts(ctx, t, conn, []string{
+		"a.com",
+		"b.com",
+		"c.com"},
+		[]tup.CertsPoliciesOrBoth{
+			tup.CertsOnly,
+			tup.PoliciesOnly,
+			tup.BothCertsAndPolicies,
+		})
+	err := conn.Close()
+	require.NoError(t, err)
+
+	// Create a test configuration for the mapserver.
+	conf := &config.Config{
+		UpdateTimer:        util.DurationWrap{Duration: 24 * time.Hour},
+		UpdateAt:           util.NewTimeOfDay(3, 0, 0, 0),
+		CTLogServerURLs:    []string{"https://ct.googleapis.com/logs/xenon2023/"},
+		DBConfig:           dbConf,
+		CertificatePemFile: "./tests/testdata/servercert.pem",
+		PrivateKeyPemFile:  "./tests/testdata/serverkey.pem",
+	}
+
+	// Mapserver:
+	server, err := mapserver.NewMapServer(ctx, conf)
+	check(err)
+
+	// Start serving requests.
+	go func() {
+		err = server.Listen(context.Background())
+		check(err)
+	}()
+
+	// Request a.com
+
+	return 0
+}
+
+func check(err error) {
+	if err != nil {
+		panic(err)
+	}
+}
