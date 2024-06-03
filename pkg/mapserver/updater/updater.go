03a194178576a8c3bb6ec184c8818ea4108e3e8b
Juan A. Garcia Pardo <juagargi@gmail.com>
2024-06-03 13:02:44 +0200

    Cleanup.

--- pkg/mapserver/updater/updater.go
+++ pkg/mapserver/updater/updater.go
@@ -405,7 +405,6 @@ func UpdateWithKeepExisting(ctx context.Context, conn db.Conn, domainNames [][]s
 		id := common.SHA256Hash32Bytes(payload)
 		policyIDs[i] = &id
 		policySubjects[i] = pol.Domain()
-		fmt.Printf("deleteme domain: %s\n", policySubjects[i])
 	}
 	// Check which policies are already present in the DB.
 	maskPols, err := conn.CheckPoliciesExist(ctx, policyIDs)
@@ -598,11 +597,9 @@ func insertCerts(ctx context.Context, conn db.Conn, names [][]string,
 	}
 
 	// Send hash, parent hash, expiration and payload to the certs table.
-	fmt.Printf("deleteme insertCerts [%s] inserting certificates ...\n", time.Now().Format(time.StampMilli))
 	if err := conn.UpdateCerts(ctx, ids, parentIDs, expirations, payloads); err != nil {
 		return fmt.Errorf("inserting certificates: %w", err)
 	}
-	fmt.Printf("deleteme insertCerts [%s] certificates inserted.\n", time.Now().Format(time.StampMilli))
 	// around 3 seconds for this ^^ (100K certs)
 
 	// Add new entries from names into the domains table iff they are leaves.
@@ -621,19 +618,15 @@ func insertCerts(ctx context.Context, conn db.Conn, names [][]string,
 		}
 	}
 	// Push the changes of the domains to the DB.
-	fmt.Printf("deleteme insertCerts [%s] updating dirty ...\n", time.Now().Format(time.StampMilli))
 	if err := conn.InsertDomainsIntoDirty(ctx, domainIDs); err != nil {
 		return fmt.Errorf("updating domains: %w", err)
 	}
-	fmt.Printf("deleteme insertCerts [%s] updating domains ...\n", time.Now().Format(time.StampMilli))
 	if err := conn.UpdateDomains(ctx, domainIDs, newNames); err != nil {
 		return fmt.Errorf("updating domains: %w", err)
 	}
-	fmt.Printf("deleteme insertCerts [%s] updating domain_certs ...\n", time.Now().Format(time.StampMilli))
 	if err := conn.UpdateDomainCerts(ctx, domainIDs, newIDs); err != nil {
 		return fmt.Errorf("updating domain_certs: %w", err)
 	}
-	fmt.Printf("deleteme insertCerts [%s] domains updated\n", time.Now().Format(time.StampMilli))
 	// around 8 seconds for this ^^ (100K certs)
 
 	return nil
