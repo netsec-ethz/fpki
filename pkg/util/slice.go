6592b9a36f395b749d721b25668333a861216170
Juan A. Garcia Pardo <juagargi@gmail.com>
2024-07-30 13:55:41 +0200

    Add function to remove several indices from slice.

--- pkg/util/slice.go
+++ pkg/util/slice.go
@@ -8,3 +8,42 @@ func RemoveElemFromSlice[T any](slice *[]T, index int) {
 	(*slice)[index] = (*slice)[N]
 	*slice = (*slice)[:N]
 }
+
+// RemoveElementsFromSlice removes the elements at indices' positions by replacing them with
+// other elements that will not be removed. For that purpose, every time we find a new index
+// to be moved, we have to find the corresponding index that will stay, starting from the end
+// of the slice.
+// The indices to remove must not repeat, must be inside bounds of the slice,
+// and in increasing order.
+func RemoveElementsFromSlice[T any](slice *[]T, indices []int) {
+	if len(indices) >= len(*slice) {
+		*slice = (*slice)[:0]
+		return
+	}
+
+	// This is the boundary starting from the end and moving to the beginning of the slice,
+	// that separates data to be removed [0,1,... N,...] from N+1 to end.
+	N := len(*slice) - 1
+
+	// Start from the beginning of the slice, pick the first element to remove, find the end-most
+	// element staying, and swap them.
+	j := len(indices) - 1 // j starts from the end of the indices.
+	i := 0
+	for ; i <= j && i < len(*slice)-len(indices); i++ { // i starts from the beginning of the indices.
+		// Find an element that will not be removed.
+		for ; j >= i; j-- {
+			// An element stays if there are no indices pointing to it; which is equivalent to
+			// find an index that is smaller than that element.
+			if indices[j] < N-i {
+				break
+			} else {
+				N--
+			}
+		}
+		// We found an element that will not be removed, swap it with N-i.
+		toStay := N - i
+		toDelete := indices[i]
+		(*slice)[toDelete] = (*slice)[toStay]
+	}
+	*slice = (*slice)[:len(*slice)-len(indices)]
+}
--- pkg/util/slice_test.go
+++ pkg/util/slice_test.go
@@ -19,3 +19,63 @@ func TestRemoveElemFromSlice(t *testing.T) {
 	require.ElementsMatch(t, slice, []int{0, 1, 3, 4, 5})
 	require.Equal(t, 0, allocs)
 }
+
+func TestRemoveElementsFromSlice(t *testing.T) {
+	testcases := map[string]struct {
+		slice    []int
+		indices  []int
+		expected []int
+	}{
+		"empty": {
+			slice:    []int{},
+			expected: []int{},
+		},
+		"single": {
+			slice:    []int{0, 1, 2},
+			indices:  []int{1},
+			expected: []int{0, 2},
+		},
+		"none": {
+			slice:    []int{0, 1, 2},
+			indices:  []int{},
+			expected: []int{0, 1, 2},
+		},
+		"head-tail": {
+			slice:    []int{0, 1, 2},
+			indices:  []int{0, 2},
+			expected: []int{1},
+		},
+		"head": {
+			slice:    []int{0, 1, 2, 3, 4, 5},
+			indices:  []int{0, 1, 2, 3},
+			expected: []int{4, 5},
+		},
+		"tail": {
+			slice:    []int{0, 1, 2, 3, 4, 5},
+			indices:  []int{2, 3, 4, 5},
+			expected: []int{0, 1},
+		},
+		"interleaved": {
+			slice:    []int{0, 1, 2, 3, 4, 5},
+			indices:  []int{0, 2, 4},
+			expected: []int{1, 3, 5},
+		},
+		"all": {
+			slice:    []int{0, 1, 2, 3, 4, 5},
+			indices:  []int{0, 1, 2, 3, 4, 5},
+			expected: []int{},
+		},
+	}
+	for name, tc := range testcases {
+		name, tc := name, tc
+		t.Run(name, func(t *testing.T) {
+			// XXX: Do not run in parallel.
+
+			allocs := tests.AllocsPerRun(func() {
+				util.RemoveElementsFromSlice(&tc.slice, tc.indices)
+			})
+			require.ElementsMatch(t, tc.slice, tc.expected)
+			require.Equal(t, 0, allocs)
+		})
+	}
+}
