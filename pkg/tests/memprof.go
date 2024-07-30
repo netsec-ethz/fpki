f19254f5e3b532da19e5c55b86d81a7b6b404874
Juan A. Garcia Pardo <juagargi@gmail.com>
2024-07-30 10:39:38 +0200

    Add comment.

--- pkg/tests/memprof.go
+++ pkg/tests/memprof.go
@@ -4,6 +4,8 @@ import (
 	"runtime"
 )
 
+// AllocsPerRunGlobal measures all the calls to malloc that happen in the process during the
+// call to the testFunc. It includes goroutines allocations and no warm up calls.
 func AllocsPerRunGlobal(testFunc func()) int {
 	var statsBefore, statsAfter runtime.MemStats
 
