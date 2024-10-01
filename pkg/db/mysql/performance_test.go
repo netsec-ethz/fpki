96a469203a7eb4fe5613bfc0d4b2c9083f437dc2
Juan A. Garcia Pardo <juagargi@gmail.com>
2024-10-01 16:38:06 +0200

    New benchmark results.

--- pkg/db/mysql/performance_test.go
+++ pkg/db/mysql/performance_test.go
@@ -314,7 +314,8 @@ func BenchmarkInsertPerformance(b *testing.B) {
 	})
 }
 
-// TestPartitionInsert tests the performance of inserting data into a certs-like table,
+// BenchmarkPartitionInsert tests the performance of inserting data into a certs-like table,
+//
 // using different approaches, in particular, partitioning the CSV file and table as well.
 //
 // Preliminary results: (running in local computer, with -short, that is, 800K certs)
@@ -340,7 +341,7 @@ func BenchmarkInsertPerformance(b *testing.B) {
 // TestPartitionInsert/load_data/myisam (77.64s)
 // TestPartitionInsert/load_data/innodb/single (89.92s)
 // TestPartitionInsert/load_data/innodb/parallel/load_0_partitions (17.03s)
-
+//
 // TestPartitionInsert/load_data/innodb/parallel/range/16 (13.46s)
 // TestPartitionInsert/load_data/innodb/parallel/range/32 (12.33s)
 // TestPartitionInsert/load_data/innodb/parallel/range/64 (12.03s)
@@ -349,19 +350,64 @@ func BenchmarkInsertPerformance(b *testing.B) {
 // TestPartitionInsert/load_data/innodb/parallel/range/sorted64 (12.20s)
 // TestPartitionInsert/load_data/innodb/parallel/range/inverse32 (11.67s)
 // TestPartitionInsert/load_data/innodb/parallel/range/inverse64 (12.00s)
-
+//
 // TestPartitionInsert/load_data/innodb/parallel/key/32 (11.64s)
 // TestPartitionInsert/load_data/innodb/parallel/key/64 (12.47s)
 // TestPartitionInsert/load_data/innodb/parallel/key/sorted32 (12.00s)
 // TestPartitionInsert/load_data/innodb/parallel/key/inverse32 (11.98s)
-
+//
 // TestPartitionInsert/load_data/innodb/parallel/linear/32 (14.00s)
 // TestPartitionInsert/load_data/innodb/parallel/linear/64 (16.70s)
 // TestPartitionInsert/load_data/innodb/parallel/linear/sorted32 (16.00s)
 // TestPartitionInsert/load_data/innodb/parallel/linear/sorted64 (14.78s)
 // TestPartitionInsert/load_data/innodb/parallel/linear/inverse32 (11.56s)
 // TestPartitionInsert/load_data/innodb/parallel/linear/inverse64 (14.02s)
-
+//
+// ------------------------  [BENCHMARK STATUS] --------------------------
+// Only for linear, two runs:
+//
+// goos: linux
+// goarch: amd64
+// pkg: github.com/netsec-ethz/fpki/pkg/db/mysql
+// cpu: Intel(R) Xeon(R) Gold 6242 CPU @ 2.80GHz
+// BenchmarkPartitionInsert/innodb/parallel/linear/2/load_only-32         	       1	47478200162 ns/op
+// BenchmarkPartitionInsert/innodb/parallel/linear/4/load_only-32         	       1	25007699646 ns/op
+// BenchmarkPartitionInsert/innodb/parallel/linear/8/load_only-32         	       1	14927805653 ns/op
+// BenchmarkPartitionInsert/innodb/parallel/linear/16/load_only-32        	       1	8559264786 ns/op
+// BenchmarkPartitionInsert/innodb/parallel/linear/32/load_only-32        	       1	4809242874 ns/op
+// BenchmarkPartitionInsert/innodb/parallel/linear/64/load_only-32        	       1	4713565653 ns/op
+// BenchmarkPartitionInsert/innodb/parallel/linear/sorted2/load_only-32   	       1	44825781781 ns/op
+// BenchmarkPartitionInsert/innodb/parallel/linear/sorted4/load_only-32   	       1	26451970662 ns/op
+// BenchmarkPartitionInsert/innodb/parallel/linear/sorted8/load_only-32   	       1	14734631174 ns/op
+// BenchmarkPartitionInsert/innodb/parallel/linear/sorted16/load_only-32  	       1	8264559184 ns/op
+// BenchmarkPartitionInsert/innodb/parallel/linear/sorted32/load_only-32  	       1	4873683350 ns/op
+// BenchmarkPartitionInsert/innodb/parallel/linear/sorted64/load_only-32  	       1	4610484153 ns/op
+// BenchmarkPartitionInsert/innodb/parallel/linear/inverse2/load_only-32  	       1	46984741498 ns/op
+// BenchmarkPartitionInsert/innodb/parallel/linear/inverse4/load_only-32  	       1	29170276264 ns/op
+// BenchmarkPartitionInsert/innodb/parallel/linear/inverse8/load_only-32  	       1	15069563037 ns/op
+// BenchmarkPartitionInsert/innodb/parallel/linear/inverse16/load_only-32 	       1	12772204752 ns/op
+// BenchmarkPartitionInsert/innodb/parallel/linear/inverse32/load_only-32 	       1	6973776232 ns/op
+// BenchmarkPartitionInsert/innodb/parallel/linear/inverse64/load_only-32 	       1	6751716890 ns/op
+//
+// BenchmarkPartitionInsert/innodb/parallel/linear/2/load_only-32         	       1	46316287235 ns/op
+// BenchmarkPartitionInsert/innodb/parallel/linear/4/load_only-32         	       1	26804651970 ns/op
+// BenchmarkPartitionInsert/innodb/parallel/linear/8/load_only-32         	       1	14177246764 ns/op
+// BenchmarkPartitionInsert/innodb/parallel/linear/16/load_only-32        	       1	8731915781 ns/op
+// BenchmarkPartitionInsert/innodb/parallel/linear/32/load_only-32        	       1	7206455873 ns/op
+// BenchmarkPartitionInsert/innodb/parallel/linear/64/load_only-32        	       1	5365581642 ns/op
+// BenchmarkPartitionInsert/innodb/parallel/linear/sorted2/load_only-32   	       1	47264175002 ns/op
+// BenchmarkPartitionInsert/innodb/parallel/linear/sorted4/load_only-32   	       1	27693145674 ns/op
+// BenchmarkPartitionInsert/innodb/parallel/linear/sorted8/load_only-32   	       1	14701929265 ns/op
+// BenchmarkPartitionInsert/innodb/parallel/linear/sorted16/load_only-32  	       1	9979814845 ns/op
+// BenchmarkPartitionInsert/innodb/parallel/linear/sorted32/load_only-32  	       1	4800589391 ns/op
+// BenchmarkPartitionInsert/innodb/parallel/linear/sorted64/load_only-32  	       1	4840803067 ns/op
+// BenchmarkPartitionInsert/innodb/parallel/linear/inverse2/load_only-32  	       1	42180543117 ns/op
+// BenchmarkPartitionInsert/innodb/parallel/linear/inverse4/load_only-32  	       1	25059406480 ns/op
+// BenchmarkPartitionInsert/innodb/parallel/linear/inverse8/load_only-32  	       1	13123920949 ns/op
+// BenchmarkPartitionInsert/innodb/parallel/linear/inverse16/load_only-32 	       1	7647975636 ns/op
+// BenchmarkPartitionInsert/innodb/parallel/linear/inverse32/load_only-32 	       1	4504448913 ns/op
+// BenchmarkPartitionInsert/innodb/parallel/linear/inverse64/load_only-32 	       1	4540372700 ns/op
+//
 // Summary:
 // - The page size was extremely important.
 // - Reconfiguring the RAID with an appropriate chunk of 64Kb was extremely important.
@@ -371,14 +417,14 @@ func BenchmarkInsertPerformance(b *testing.B) {
 // does NOT help performance; it doesn't hurt it either.
 // We can use sorting to assign IDs to different workers, to avoid
 // dead-locks for large multi-inserts.
-
+//
 // We decide to use 32 partitions, linear key, inverse sorted data, as we anyway will be "sorting"
 // (aka dispatching) the data to avoid deadlocks.
 // The reason that the inverse sorted data is faster might be because each thread will attack
 // many partitions, while straight sorted data means each thread uses just one.
 // We could use KEY for partitions, but the data is sorted anyway.
 func BenchmarkPartitionInsert(b *testing.B) {
-	ctx, cancelF := context.WithTimeout(context.Background(), 2*time.Minute)
+	ctx, cancelF := context.WithTimeout(context.Background(), 10*time.Minute)
 	defer cancelF()
 
 	// Configure a test DB.
@@ -1154,7 +1200,7 @@ func writeChunkedCsv(
 		go func() {
 			defer wg.Done()
 			writeCSV(t, fileNameFunc(w), chunk)
-			fmt.Printf("[%2d] wrote %d records\n", w, len(chunk))
+			t.Logf("[%2d] wrote %d records\n", w, len(chunk))
 		}()
 	}
 	wg.Wait()
@@ -1224,7 +1270,7 @@ func writeSortedChunkedCsv(
 			defer wg.Done()
 			// We are done with this worker.
 			writeCSV(t, fName(w), chunk)
-			fmt.Printf("[%2d] wrote %d records\n", w, len(chunk))
+			t.Logf("[%2d] wrote %d records\n", w, len(chunk))
 		}()
 	}
 	wg.Wait()
