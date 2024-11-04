62574884ba3d05fdafee687c41882404bb5c8d69
Juan A. Garcia Pardo <juagargi@gmail.com>
2024-11-04 10:56:01 +0100

    The csv splitter stage streams out lines from the file.

--- cmd/ingest/csvSplitWorker.go
+++ cmd/ingest/csvSplitWorker.go
@@ -3,10 +3,10 @@ package main
 import (
 	"encoding/csv"
 	"fmt"
+	"io"
 
 	pip "github.com/netsec-ethz/fpki/pkg/pipeline"
 	"github.com/netsec-ethz/fpki/pkg/util"
-	"go.opentelemetry.io/otel/attribute"
 )
 
 type line struct {
@@ -25,77 +25,72 @@ func (l line) String() string {
 // memory when requested. This requires a change in pkg/pipeline.
 type csvSplitWorker struct {
 	*pip.Stage[util.CsvFile, line]
-
-	channelsCache []int // reuse storage
+	lines   chan line
+	lastErr error
 }
 
 func NewCsvSplitWorker(p *Processor) *csvSplitWorker {
 	w := &csvSplitWorker{
-		channelsCache: make([]int, 0),
+		lines:   make(chan line, 1024), // Cache 1K lines.
+		lastErr: nil,
 	}
+
+	lastOut := make([]line, 1)
+	lastOutIndex := make([]int, 1) // The last parser used.
+	var stillLinesToSend bool
 	w.Stage = pip.NewStage[util.CsvFile, line](
 		"csv_split",
 		pip.WithMultiOutputChannels[util.CsvFile, line](p.NumWorkers),
-		pip.WithProcessFunction(
-			func(in util.CsvFile) ([]line, []int, error) {
-				ctx, span := w.Tracer.Start(w.Stage.Ctx, "csv_split-new-file")
-				w.Stage.Ctx = ctx
-				defer span.End()
-				span.SetAttributes(
-					attribute.String("file-name", in.Filename()),
-				)
-
-				p.stats.TotalFilesRead.Add(1)
-				// Split the file into multiple lines.
-				lines, err := w.splitFile(in)
-				if err != nil {
-					return nil, nil, err
-				}
-				span.SetAttributes(
-					attribute.Int("lines", len(lines)),
-				)
-
-				// Prepare the staggered fan-out list of output channels.
-				w.channelsCache = w.channelsCache[:0]
-				for i := 0; i < len(lines); i++ {
-					w.channelsCache = append(w.channelsCache, i%p.NumWorkers)
-				}
-
-				return lines, w.channelsCache, nil
-			},
-		),
+		pip.WithProcessFunction(func(in util.CsvFile) ([]line, []int, error) {
+			err := w.startReadingLines(in)
+			if err == nil {
+				err = pip.StreamOutput
+			}
+			return lastOut[:0], lastOutIndex[:0], err
+		}),
+		pip.WithOutputStreamingFunction[util.CsvFile](func(outs *[]line, outChs *[]int) error {
+			*outs = lastOut
+			*outChs = lastOutIndex
+			(*outs)[0], stillLinesToSend = <-w.lines
+			if !stillLinesToSend {
+				*outs = (*outs)[:0]
+				*outChs = (*outChs)[:0]
+				return nil
+			}
+			(*outChs)[0] = ((*outChs)[0] + 1) % p.NumWorkers
+			return pip.StreamOutput
+		}),
 	)
 	return w
 }
 
-func (w *csvSplitWorker) splitFile(f util.CsvFile) ([]line, error) {
+func (w *csvSplitWorker) startReadingLines(f util.CsvFile) error {
 	fileReader, err := f.Open()
 	if err != nil {
-		return nil, err
+		return err
 	}
 
 	r := csv.NewReader(fileReader)
 	r.FieldsPerRecord = -1 // don't check number of fields
-
-	// TODO: try to reuse storage by calling r.ReuseRecord and copying.
-	records, err := r.ReadAll()
-	if err != nil {
-		return nil, err
-	}
-
-	// Close the csv file.
-	if err := f.Close(); err != nil {
-		return nil, err
-	}
-
-	// Convert records to lines (cast each line).
-	lines := make([]line, len(records))
-	for i, l := range records {
-		lines[i] = line{
-			fields: l,
-			number: i + 1,
+	var records []string
+	w.lines = make(chan line, cap(w.lines))
+	go func() {
+		for lineNo := 1; ; lineNo++ {
+			records, err = r.Read()
+			if err != nil {
+				if err != io.EOF {
+					w.lastErr = err
+				}
+				break
+			}
+			w.lines <- line{
+				fields: records,
+				number: lineNo,
+			}
 		}
-	}
+		close(w.lines)
+		f.Close()
+	}()
 
-	return lines, nil
+	return nil
 }
